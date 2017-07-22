8995b41f7f19bcadb5355e07820f3f02b7f693ae
Austen Daniel Anop <austen626@outlook.com>
2017-07-22 09:04:02 -0500

    Fixed summary screen

--- src/screens/Sales/2.0/summary.js
+++ src/screens/Sales/2.0/summary.js
@@ -818,7 +818,6 @@ function AddDealer(props) {
                 <form action="javascript:void(0)" onSubmit={(e) => handleSubmit(e)} noValidate>
                     <div className="container black-box">
                         <div className="table-div">
-
                             <table className="summary-row">
                                 <tr>
                                     <td><span><b>Name: </b> {customer.main_app.name}</span></td>
@@ -845,7 +844,8 @@ function AddDealer(props) {
                                     <td><span>{customer.co_enabled ? customer.co_app.cell_phone : ''}</span></td>
                                 </tr>
                             </table>
-
+                        </div>
+                        <div className="table-div status">
                             <div className="row other-details summary-row">
                                 <div className="col">
                                     <span className="status">Credit application</span>
@@ -881,7 +881,6 @@ function AddDealer(props) {
                                     }
                                 </div>
                             </div>
-
                         </div>
                     </div>
                     <div className="footer-container">
